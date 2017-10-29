c644a80905694f02e2b6883a22cc24db5dac8d7d
Mason Leary <89mleary@gmail.com>
2017-10-29 17:59:54 -0600

    Burger

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,15 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.en.JAWSDB_url);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "burgers_db"
+  });
+};
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
